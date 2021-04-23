a0eb469df2ed5bf0e20d2089688c877a597f23ed
roshoyt <rosshoytmusic@gmail.com>
2021-04-22 20:58:54 -0700

    Added command-line args support to specify domain name, changed PingClient.py

--- PingClient.py
+++ PingClient.py
@@ -1,11 +1,14 @@
 import socket
+import os, sys
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 2000        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    # Process the command line arguments, if none provided use 'google.com'
+    data = sys.argv[1] if len(sys.argv) > 1 else 'google.com'
+    s.sendall(bytes(data, 'utf-8'))
     data = s.recv(1024)
 
 print('Received', repr(data))
