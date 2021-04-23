f618da42eb953b32063cb67e28ba616372840d39
roshoyt <rosshoytmusic@gmail.com>
2021-04-23 10:50:10 -0700

    Changed ping function to run 20 times, store and return results, changed PingServer.py

--- PingServer.py
+++ PingServer.py
@@ -6,19 +6,19 @@ import socket
 HOST = '127.0.0.1'
 PORT = 2000
 
-def ping(host):
+def ping(hostToPing):
     """
-    Returns True if host (str) responds to a ping request.
-    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    Pings the requested domain-name 20 times
+    Prints and returns the results of the ping request, including any error messages
     """
+    
+    # Ping the domain 20 times and store the result
+    result = subprocess.run(['ping', '-c', '20', '-n', hostToPing], stderr=subprocess.PIPE, encoding='utf-8')
 
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower()=='windows' else '-c'
+    print(result.stdout)
+    print(result.stderr)
 
-    # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host]
-
-    return subprocess.call(command) == 0
+    return result
 
 
 
@@ -26,13 +26,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
-    with conn:
+    with conn: 
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
             if not data:
                 break
             pingResult = ping(data) 
-            #conn.sendall(bytes(pingResult, 'utf-8'))
+            conn.sendall(bytes('all set', 'utf-8'))
 
 
