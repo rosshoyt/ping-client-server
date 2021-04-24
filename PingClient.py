4169ffe805cd05d641c1904cc9fe3b0239484834
roshoyt <rosshoytmusic@gmail.com>
2021-04-24 15:17:48 -0700

    Cleaned up processing of command line args in PingClient.py

--- PingClient.py
+++ PingClient.py
@@ -4,19 +4,18 @@ import json
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 10800        # The port used by the server
-userArgs = sys.argv[1]
+
+# Process the command line arguments, if none provided use 'google.com'
+domainName = sys.argv[1] if len(sys.argv[1]) > 1 else 'google.com'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    # Process the command line arguments, if none provided use 'google.com'
-    data = userArgs if len(sys.argv) > 1 else 'google.com'
-    s.sendall(bytes(data, 'utf-8'))
+    s.sendall(bytes(domainName, 'utf-8'))
     data = s.recv(1024)
 
 # decode data
 strData = data.decode("utf-8")
-
-usrMessageStart = 'Ping ' + userArgs + ' Results: '
+usrMessageStart = 'Ping ' + domainName + ' Results: '
 if 'ERROR' in strData:
     print(usrMessageStart, 'error - could not connect to provided domain name')
 else:
