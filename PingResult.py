452e8aa0042c007dcaf41ca3ea60fae0104d8350
roshoyt <rosshoytmusic@gmail.com>
2021-04-24 16:46:33 -0700

    Added constructor, default args to class PingResult.py

--- PingResult.py
+++ PingResult.py
@@ -1,8 +1,8 @@
 class PingResult:
-    domainName = 'unset'
-    error = 'unset'
-    numPacketsTransmitted = 0
-    numPacketsRecieved = 0
-    percentPacketLoss = -1
-    averageRTT = -1
-
+    def __init__(self, domainName='', errorMsg='', numPacketsTransmitted=-1, numPacketsRecieved=-1, percentPacketLoss=-1, averageRTT=-1 ):
+        self.domainName = domainName
+        self.errorMsg = errorMsg
+        self.numPacketsTransmitted = numPacketsTransmitted
+        self.numPacketsRecieved = numPacketsRecieved
+        self.percentPacketLoss = percentPacketLoss
+        self.averageRTT = averageRTT
