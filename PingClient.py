f0dbc0e1f6669cb7f565e6d6631c9c78cef865d9
roshoyt <rosshoytmusic@gmail.com>
2021-04-24 22:03:37 -0700

    Fixed error parsing command line args in PingClient.py

--- PingClient.py
+++ PingClient.py
@@ -7,7 +7,7 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 10800        # The port used by the server
 
 # Process the command line arguments, if none provided use 'google.com'
-domainName = sys.argv[1] if len(sys.argv[1]) > 1 else 'google.com'
+domainName = sys.argv[1] if len(sys.argv) > 1 else 'google.com'
 
 # Start the socket, connect to the server and send a request
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
