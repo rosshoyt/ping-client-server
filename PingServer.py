f68a5df84948fb9460d4d4e37cd3d81a0ad69322
roshoyt <rosshoytmusic@gmail.com>
2021-04-24 18:32:04 -0700

    Added error message parsing to function parsePingResults in PingServer.py

--- PingServer.py
+++ PingServer.py
@@ -13,26 +13,29 @@ def parsePingResult(result):
     """
     TODO handle error message in std err
     """
-
     pingResult = PingResult()
     
-    pingResult.numPacketsTransmitted = re.search("(\d+)(?=\s*packets transmitted)", result.stdout).group(0)
-    pingResult.numPacketsRecieved = re.search("(\d+)(?=\s*received)", result.stdout).group(0)
-    pingResult.percentPacketLoss = re.search("(\d+)(?=\s*% packet loss)", result.stdout).group(0)
+    if len(result.stderr) == 0:
+        pingResult.numPacketsTransmitted = re.search("(\d+)(?=\s*packets transmitted)", result.stdout).group(0)
+        pingResult.numPacketsRecieved = re.search("(\d+)(?=\s*received)", result.stdout).group(0)
+        pingResult.percentPacketLoss = re.search("(\d+)(?=\s*% packet loss)", result.stdout).group(0)
 
-    # Get decimal number before second to last slash (averageRTT)
-     # TODO get averageRTT from regex, e.g (\d+)(?=.*?\/.*)
-    slashCount = 0
-    charStack = []
-    for c in reversed(result.stdout):
-        if c == '/':
-            slashCount += 1
-        elif slashCount == 2:
-            charStack.insert(0,c)
-        if slashCount == 3:
-            break
-    
-    pingResult.averageRTT = ''.join(charStack)        
+        # Get decimal number before second to last slash (averageRTT)
+        # TODO get averageRTT from regex, e.g (\d+)(?=.*?\/.*)
+        slashCount = 0
+        charStack = []
+        for c in reversed(result.stdout):
+            if c == '/':
+                slashCount += 1
+            elif slashCount == 2:
+                charStack.insert(0,c)
+            if slashCount == 3:
+                break
+        
+        pingResult.averageRTT = ''.join(charStack)      
+        
+    else:        
+          pingResult.errorMsg = re.search('(?<=ping: ).*$',result.stderr).group(0)
 
     return json.dumps(pingResult.__dict__)
 
